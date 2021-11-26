b26d211fd6772a80d3f28c0191b837f78b15a2dd
Kyle Geske <stungeye@gmail.com>
2021-11-26 09:52:27 -0600

    Defined the MotionSprite class and its public API.

--- src/ofApp.h
+++ src/ofApp.h
@@ -30,6 +30,22 @@ public:
 		ofDrawRectangle(position.x, position.y, width, height);
 		ofPopStyle();
 	}
+};
+
+class MotionSprite : public Sprite {
+private:
+	glm::vec2 velocity;
+public:
+	MotionSprite(float xPosition, float yPosition, float width, float height,
+	             float xSpeed, float ySpeed)
+		: Sprite{xPosition, yPosition, width, height}, velocity{xSpeed, ySpeed} {
+	}
+
+	void cruiseAt(glm::vec2 newVelocity); // A setter for velocity.
+	void accelerate(glm::vec2 impulse); // This impulse will be applied to the velocity.
+	void move(float deltaTime);
+	void bounceHorizontalWithEdge(float ceilingY, float floorY);
+	void bounceVerticalWith(Sprite other);
 
 };
 
@@ -60,7 +76,10 @@ private:
 	const int paddleWidth{20};
 	const int paddleHeight{100};
 	const int paddleEdgeBuffer{50};
+	const int ballWidth{20};
+	const int ballHeight{20};
 
 	Sprite p1Paddle{0, 0, paddleWidth, paddleHeight};
 	Sprite p2Paddle{0, 0, paddleWidth, paddleHeight};
+	MotionSprite ball{0, 0, ballWidth, ballHeight, 0, 0};
 };
